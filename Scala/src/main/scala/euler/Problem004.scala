c9416badbbc95412db8ef572d9124f10c13b408b
Dathan Bennett <dbennett@palantir.com>
2013-10-18 20:58:44 -0700

    Update packages and fix error in Problem005

--- Scala/src/main/scala/euler/Problem004.scala
+++ Scala/src/main/scala/euler/Problem004.scala
@@ -1,29 +1,29 @@
-package projecteuler.p004
+package euler
 
 // Project Euler
 // Problem 4
 //
-// A palindromic number reads the same both ways. The largest palindrome made from the product 
+// A palindromic number reads the same both ways. The largest palindrome made from the product
 // of two 2-digit numbers is 9009 = 91  99.
-// 
+//
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
-object main {
+object Problem004 {
 	def reverseString(str: String) = {
 		str.foldLeft("")((a, e)=> e+a)
 	}
-	
+
 	def isPalindromic(n: Int) = {
 		n.toString() == reverseString(n.toString())
 	}
-	
+
 	def nums(a: Int, b: Int): Stream[Int] = {
 		if (b < 100)
 			if (a < 100) return Stream.Empty
 			else return Stream.cons(a*b, nums(a-1, 999));
 		else return Stream.cons(a*b, nums(a, b-1));
 	}
-	
+
 	def main(args: Array[String]) = {
 		println(nums(999, 999).filter(x => isPalindromic(x)).max)
 	}
--- Scala/src/main/scala/euler/Problem005.scala
+++ Scala/src/main/scala/euler/Problem005.scala
@@ -1,23 +1,23 @@
-package projecteuler.p005
+package euler
 
 // Project Euler
 // Problem 5
 //
-// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without 
+// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without
 // any remainder.
-// 
+//
 // What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-object main {
+object Problem005 {
 	def nums(x: Int): Stream[Int] = {
 		return Stream.cons(x, nums(x+1))
 	}
-	
+
 	def isMultiple(x: Int, items: List[Int]): Boolean = {
 		items.forall(i => x%i==0)
 	}
 
 	def main(args: Array[String]) = {
-		println(nums(2).filter(x => isMultiple(x, List.range(2, 21))).first)
+		println(nums(2).filter(x => isMultiple(x, List.range(2, 21))) take 1)
 	}
 }
