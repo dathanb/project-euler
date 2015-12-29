3f8673dfaef28f68280f07b78f31e2b40ab0f9c1
Dathan Bennett <dathan@Dathans-MacBook-Pro.local>
2015-12-28 21:34:00 -0800

    Add IntelliJ projects for problems 1-6, and add Problem 7

--- Scala/Problem 001/src/projecteuler/p001/Problem001.scala
+++ Scala/Problem 001/src/projecteuler/p001/Problem001.scala
@@ -8,9 +8,9 @@ package projecteuler.p001
 //
 // Find the sum of all the multiples of 3 or 5 below 1000.
 object Problem001 {
-	def main(args: Array[String]){
-		println(List.range(1, 1000)
-		    .filter(n => n%3==0 || n%5==0)
-		    .foldLeft(0)((a,i)=>a+i))
-	}
+  def main(args: Array[String]) {
+    println(List.range(1, 1000)
+      .filter(n => n % 3 == 0 || n % 5 == 0)
+      .foldLeft(0)((a, i) => a + i))
+  }
 }
