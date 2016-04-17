c72220aa0fe7970017f9fb43f4dd47aa74306d5e
Dathan Bennett <dathan@Dathans-MacBook-Pro.local>
2016-04-17 16:56:44 -0700

    Add IntelliJ project

---
+++ Java/src/main/java/org/dathan/euler/problem064/Problem064.java
@@ -0,0 +1,13 @@
+package org.dathan.euler.problem064;
+
+public class Problem064 {
+    public static void main(String... args) {
+        new Problem064().run();
+    }
+
+    public void run() {
+
+    }
+}
+
+
